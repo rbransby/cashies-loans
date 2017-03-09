cacd79a07111936405ca619909177f9a234ade01
Hassan Ali <hassan.ali@isobar.com>
2017-03-09 12:05:03 +1100

    explicit lossy image minifcation

--- gulp/images.js
+++ gulp/images.js
@@ -1,9 +1,16 @@
 const config =            require('../config');
+const imageminMozjpeg =   require('imagemin-mozjpeg');
+const imageminGifsicle =  require('imagemin-gifsicle');
+const imageminPngquant =  require('imagemin-pngquant');
 
 module.exports = (gulp, $) => {
   return () => {
     return gulp.src(`${config.paths.images}/**/*`)
-      .pipe($.cache($.imagemin()))
+      .pipe($.imagemin([
+        imageminMozjpeg({ quality:80 }),
+        imageminGifsicle({ optimizationLevel: 3 }),
+        imageminPngquant({ quality: 80 })],
+        {verbose: true}))
       .pipe(gulp.dest(`${config.paths.dist}/images`));
   };
 };
